11bce8e4b937f4cbc5d391911f29340f9b065e53
Albert Ventura <albert@venturads.com>
2018-04-28 02:16:25 +0000

    fix folders

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  //response.send('Hello from express')
+  res.sendfile('index.html'); //load
+})
+app.listen(port, (err) => {
+  if(err){
+    console.log('we got bugs', err); 
+  }
+  console.log('server gots ears on ' + port);
+});
